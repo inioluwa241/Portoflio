5a3f236a4221c8ddf20608f860d73097adbe7f45
inioluwa <samf72302@gmail.com>
2025-09-23 00:36:05 +0100

    fixing build issues

--- src/app/Components/Skills/BackendSkills.js
+++ src/app/Components/Skills/BackendSkills.js
@@ -8,16 +8,15 @@ const BackendSkills = () => {
 
   return (
     <Card
-      style={
-        {
-          // display: "flex",
-          // flex: `0 0 ${isMobile ? "30%" : "50%"}`,
-          // gap: "32px",
-          // justifyContent: "center",
-          // flexDirection: "column",
-          // backgroundColor: "#080c14",
-        }
-      }
+      style={{
+        display: "flex",
+        flex: `0 0 ${isMobile ? "30%" : "50%"}`,
+
+        gap: "32px",
+        justifyContent: "center",
+        flexDirection: "column",
+        backgroundColor: "#080c14",
+      }}
     >
       <h3
         style={{
